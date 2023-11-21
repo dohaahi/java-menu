204859bbca288a8247bb2c9e5d839f629b07ff9f
doha <dohaahi@naver.com>
2023-11-22 01:19:19 +0900

    feat (InputView): 못 먹는 메뉴 입력 받는 기능 구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,6 +1,7 @@
 package menu.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static menu.validator.MenuValidator.inputMenuValidate;
 import static menu.validator.NameValidator.inputNameValidate;
 
 import java.util.Arrays;
@@ -10,6 +11,7 @@ import java.util.stream.Collectors;
 public class InputView {
 
     public static final String NAME_DELIMITER = ",";
+    public static final String MENU_DELIMITER = ",";
     public static final String START_GAME_MESSAGE = "점심 메뉴 추천을 시작합니다.";
     public static final String REQUEST_NAME_MESSAGE = "코치의 이름을 입력해 주세요. (" + NAME_DELIMITER + " 로 구분)";
     public static final String REQUEST_CANNOT_EAT_MENU_MESSAGE = "%s(이)가 못 먹는 메뉴를 입력해 주세요.";
@@ -22,6 +24,16 @@ public class InputView {
         inputNameValidate(input);
 
         return Arrays.stream(input.split(","))
+                .collect(Collectors.toList());
+    }
+
+    public List<String> readCanNotEatMenus() {
+        System.out.println(REQUEST_CANNOT_EAT_MENU_MESSAGE);
+
+        String input = readLine().trim();
+        inputMenuValidate(input);
+
+        return Arrays.stream(input.split(MENU_DELIMITER))
                 .collect(Collectors.toList());
     }
 }
