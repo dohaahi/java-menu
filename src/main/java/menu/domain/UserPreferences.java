33051335d43a6f6edeb1f141cf1796683bbbfadb
doha <dohaahi@naver.com>
2023-11-22 02:10:38 +0900

    feat (UserPreferences): 사용자 정보 담는 객체 생성

---
+++ src/main/java/menu/domain/UserPreferences.java
@@ -0,0 +1,17 @@
+package menu.domain;
+
+import java.util.List;
+
+public class UserPreferences {
+    private final List<String> names;
+    private final List<String> menus;
+
+    public UserPreferences(List<String> names, List<String> menus) {
+        this.names = names;
+        this.menus = menus;
+    }
+
+    public void recommendMenu() {
+
+    }
+}
