67e35f861969fccccee7e934d0aac6907918c9d9
doha <dohaahi@naver.com>
2023-11-22 02:16:47 +0900

    refactor (IllegalNameException): 예외 메세지 수정

--- src/main/java/menu/exception/IllegalNameException.java
+++ src/main/java/menu/exception/IllegalNameException.java
@@ -1,7 +1,7 @@
 package menu.exception;
 
 public class IllegalNameException extends IllegalArgumentException {
-    private static final String EXCEPTION_MESSAGE = "코치의 이름 입력이 잘못되었습니다. 다시 입력해 주세요.";
+    private static final String EXCEPTION_MESSAGE = "코치의 이름이 잘못 입력되었습니다. 다시 입력해 주세요.";
 
     public IllegalNameException() {
         super(EXCEPTION_MESSAGE);
