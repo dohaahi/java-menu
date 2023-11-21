f31ec29afc3303e85d1233e2c36e8fd55746449e
doha <dohaahi@naver.com>
2023-11-22 02:10:14 +0900

    feat (FunctionalInterfaces): 예외 발생시 재입력 할 수 있는 기능 구현

---
+++ src/main/java/menu/util/FunctionalInterfaces.java
@@ -0,0 +1,15 @@
+package menu.util;
+
+import java.util.function.Supplier;
+
+public class FunctionalInterfaces {
+    public static <T> T retryIfFailure(final Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+    }
+}
