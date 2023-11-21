1eabd945819dcaf1b348569870913f1ab586851e
doha <dohaahi@naver.com>
2023-11-22 00:59:53 +0900

    feat (NameValidator): 이름 검증 기능 구현

---
+++ src/main/java/menu/validator/NameValidator.java
@@ -0,0 +1,36 @@
+package menu.validator;
+
+import static menu.view.InputView.NAME_DELIMITER;
+
+import exception.IllegalNameException;
+import java.util.regex.Pattern;
+
+public class NameValidator {
+    private static final String NAME_REGEX = "^[가-힣]+(" + NAME_DELIMITER + "[가-힣]+)*$";
+
+    public static void inputNameValidate(final String input) {
+        validateNameEmpty(input);
+        validateNameMatchedNameRegex(input);
+        validateNameDelimiter(input);
+    }
+
+    private static void validateNameEmpty(final String input) {
+        if (input.isBlank()) {
+            throw new IllegalNameException();
+        }
+    }
+
+    private static void validateNameMatchedNameRegex(final String input) {
+        if (!Pattern.matches(NAME_REGEX, input)) {
+            throw new IllegalNameException();
+        }
+    }
+
+    private static void validateNameDelimiter(final String input) {
+        try {
+            input.split(",");
+        } catch (Exception exception) {
+            throw new IllegalNameException();
+        }
+    }
+}
