7f0a2d2bf2496fd8993b2e8db4a12b35730caafa
doha <dohaahi@naver.com>
2023-11-22 00:56:52 +0900

    feat (IllegalMenuException): 메뉴 예외처리 객체 구현

---
+++ src/main/java/exception/IllegalMenuException.java
@@ -0,0 +1,9 @@
+package exception;
+
+public class IllegalMenuException extends IllegalArgumentException {
+    private static final String EXCEPTION_MESSAGE = "메뉴 입력이 잘못되었습니다. 다시 입력해 주세요.";
+
+    public IllegalMenuException() {
+        super(EXCEPTION_MESSAGE);
+    }
+}
